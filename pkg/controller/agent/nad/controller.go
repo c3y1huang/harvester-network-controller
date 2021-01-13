aaeedb388fbf4298079f7d35b2e621448590560a
futuretea <1913508671@qq.com>
2021-01-13 10:15:06 +0800

    revert nad controller

--- pkg/controller/agent/nad/controller.go
+++ pkg/controller/agent/nad/controller.go
@@ -2,10 +2,15 @@ package nad
 
 import (
 	"context"
+	"fmt"
 
 	nadv1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
+	ctlharv1 "github.com/rancher/harvester/pkg/generated/controllers/harvester.cattle.io/v1alpha1"
+	"k8s.io/klog"
 
+	"github.com/rancher/harvester-network-controller/pkg/bridge"
 	"github.com/rancher/harvester-network-controller/pkg/config"
+	"github.com/rancher/harvester-network-controller/pkg/util"
 )
 
 const (
@@ -13,6 +18,7 @@ const (
 )
 
 type Handler struct {
+	settingCache ctlharv1.SettingCache
 }
 
 func Register(ctx context.Context, management *config.Management) error {
@@ -35,6 +41,25 @@ func (h Handler) OnChange(key string, nad *nadv1.NetworkAttachmentDefinition) (*
 		return nad, nil
 	}
 
+	klog.Infof("nad configuration %s has been changed: %s", nad.Name, nad.Spec.Config)
+	netconf, err := util.DecodeNetConf(nad.Spec.Config)
+	if err != nil {
+		return nil, err
+	}
+
+	l, err := h.getLink()
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO delete previous vlan id when update nad
+
+	if l != nil {
+		if err := l.AddBridgeVlan(uint16(netconf.Vlan)); err != nil {
+			return nil, err
+		}
+	}
+
 	return nad, nil
 }
 
@@ -47,5 +72,37 @@ func (h Handler) OnRemove(key string, nad *nadv1.NetworkAttachmentDefinition) (*
 		return nad, nil
 	}
 
+	klog.Infof("nad configuration %s has been deleted.", nad.Name)
+	netconf, err := util.DecodeNetConf(nad.Spec.Config)
+	if err != nil {
+		return nil, err
+	}
+
+	l, err := h.getLink()
+	if err != nil {
+		return nil, err
+	}
+
+	if l != nil {
+		klog.Infof("remove nad %s with vid %d from the list", key, netconf.Vlan)
+		if err := l.DelBridgeVlan(uint16(netconf.Vlan)); err != nil {
+			return nil, err
+		}
+	}
+
 	return nad, nil
+}
+
+func (h Handler) getLink() (*bridge.Link, error) {
+	setting, err := h.settingCache.Get(util.NetworkSettingName)
+	if err != nil {
+		return nil, fmt.Errorf("get vlan setting failed, error: %+v", err)
+	}
+
+	networkSetting, err := util.DecodeNetworkSettings(setting.Value)
+	if err != nil {
+		return nil, err
+	}
+
+	return bridge.GetLink(networkSetting.NIC)
 }
